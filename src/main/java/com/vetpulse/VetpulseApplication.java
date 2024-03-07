c5daf9d26b770bc40722db6fa9cd85f312a56e04
Cesar Arce <66699731+Argom23@users.noreply.github.com>
2024-03-07 17:09:05 -0600

    Finalizacion de Semana 05

---
+++ src/main/java/com/vetpulse/VetpulseApplication.java
@@ -0,0 +1,13 @@
+package com.vetpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetpulse/VetpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
