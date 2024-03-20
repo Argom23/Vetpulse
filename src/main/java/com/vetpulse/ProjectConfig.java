41742c81f91e41a16d032ce7fc6d1f6002b6993d
Cesar Arce <66699731+Argom23@users.noreply.github.com>
2024-03-20 16:40:57 -0600

    Actualizacion

--- src/main/java/com/vetpulse/ProjectConfig.java
+++ src/main/java/com/vetpulse/ProjectConfig.java
@@ -46,4 +46,11 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+    
+    
+    
+    
+    
+    
+    
 }
